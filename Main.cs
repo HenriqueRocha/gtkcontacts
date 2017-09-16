53d3bea8a35696369bb633375f291bb547c68951
Henrique Rocha <hmrocha@gmail.com>
2017-09-17 00:20:55 +0200

    Arrange Widgets Using Boxes

--- Main.cs
+++ Main.cs
@@ -5,21 +5,49 @@ class MainClass {
 	public static void Main (string[] args)
 	{
 		Application.Init();
+		SetUpGui ();
+		Application.Run ();
+	}
 
-		Window w = new Window ("Gtk# Basics");
-		Button b = new Button ("Hit me");
+	static void SetUpGui ()
+	{
+		Window w = new Window ("Gtk# Contacts");
 
-		// set up event handling: verbose to illustrate
-		// the use of delegates.
-		w.DeleteEvent += new DeleteEventHandler (Window_Delete);
+		VBox v = new VBox();
+		v.BorderWidth = 6;
+		v.Spacing = 6;
+		w.Add(v);
+
+		HBox h = new HBox ();
+		h.BorderWidth = 6;
+		h.Spacing = 6;
+		v.PackStart(h, false, false, 0);
+
+		Button b = new Button ("Save");
 		b.Clicked += new EventHandler (Button_Clicked);
+		v.PackStart(b, false, false, 0);
 
-		// initialize the GUI
-		w.Add (b);
-		w.SetDefaultSize (200, 100);
+		v = new VBox ();
+		v.Spacing = 6;
+		h.PackStart (v, false, false, 0);
+
+		Label l = new Label ("Full name: ");
+		l.Xalign = 0;
+		v.PackStart (l, true, false, 0);
+
+		l = new Label ("Email address: ");
+		l.Xalign = 0;
+		v.PackStart (l, true, false, 0);
+
+		v = new VBox ();
+		v.Spacing = 6;
+		h.PackStart (v, true, true, 0);
+
+		v.PackStart (new Entry(), true, true, 0);
+		v.PackStart (new Entry(), true, true, 0);
+
+		w.DeleteEvent += new DeleteEventHandler (Window_Delete);
 		w.ShowAll ();
-
-		Application.Run();
 	}
 
 	static void Window_Delete (object o, DeleteEventArgs args)

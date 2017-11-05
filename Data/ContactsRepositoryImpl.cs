13fdf350c77367b37e794284b6019acae55e1e15
Henrique Rocha <hmrocha@gmail.com>
2017-11-05 23:44:23 +0100

    Load contacts from xml.

--- Data/ContactsRepositoryImpl.cs
+++ Data/ContactsRepositoryImpl.cs
@@ -6,13 +6,26 @@ namespace ContactsSharp.Data
 {
 	public class ContactsRepositoryImpl : ContactsRepository
 	{
+		private const String NAME_TAG = "name";
+		private const String EMAIL_TAG = "email";
+		private const String TAGS_TAG = "tags";
+
 		private string filename;
 		private ContactList contactList;
 
 		public ContactsRepositoryImpl(string filename)
 		{
 			this.filename = filename;
-			contactList = loadContacts();
+
+			if (!File.Exists(filename))
+			{
+				using (TextWriter writer = File.CreateText(filename)) { }
+				contactList = new ContactList();
+			}
+			else
+			{
+				contactList = loadContacts();
+			}
 		}
 
 		public ContactList getContacts()
@@ -27,7 +40,8 @@ namespace ContactsSharp.Data
 
 		public void Save()
 		{
-			using (XmlTextWriter xmlTextWriter = new XmlTextWriter(File.CreateText(filename))) {
+			using (XmlTextWriter xmlTextWriter = new XmlTextWriter(File.CreateText(filename)))
+			{
 				xmlTextWriter.Formatting = Formatting.Indented;
 				xmlTextWriter.Indentation = 2;
 				xmlTextWriter.IndentChar = ' ';
@@ -36,9 +50,9 @@ namespace ContactsSharp.Data
 				{
 					Contact c = contactList.Get(i);
 					xmlTextWriter.WriteStartElement("contact");
-					xmlTextWriter.WriteElementString("name", c.Fullname);
-					xmlTextWriter.WriteElementString("email", c.Email);
-					xmlTextWriter.WriteElementString("tags", c.Tags);
+					xmlTextWriter.WriteElementString(NAME_TAG, c.Fullname);
+					xmlTextWriter.WriteElementString(EMAIL_TAG, c.Email);
+					xmlTextWriter.WriteElementString(TAGS_TAG, c.Tags);
 					xmlTextWriter.WriteEndElement();
 				}
 				xmlTextWriter.WriteEndElement();
@@ -49,41 +63,76 @@ namespace ContactsSharp.Data
 		{
 			ContactList loadedContacts = new ContactList();
 
-			if (!File.Exists(filename))
+			using (XmlTextReader reader = new XmlTextReader(File.OpenText(filename)))
 			{
-				using (TextWriter writer = File.CreateText(filename)) { }
-			}
-
-			try
-			{
-				using (StreamReader reader = File.OpenText(filename))
-				{
-					string line = null;
-					while ((line = reader.ReadLine()) != null)
-					{
-						string[] split = line.Split(',');
-						Contact contact = new Contact(split[0], split[1], split[2]);
-						loadedContacts.Add(contact);
-					}
-				}
-			}
-			catch (IndexOutOfRangeException e)
-			{
-				Console.Error.WriteLine("No filename specified.");
-				Environment.Exit(1);
-			}
-			catch (FileNotFoundException e)
-			{
-				Console.Error.WriteLine("File \"{0}\" does not exist.", filename);
-				Environment.Exit(2);
-			}
-			catch (Exception e)
-			{
-				Console.Error.WriteLine(e);
-				Environment.Exit(3);
+				loadedContacts = ReadContacts(reader);
 			}
 
 			return loadedContacts;
+		}
+
+		private ContactList ReadContacts(XmlTextReader reader)
+		{
+			ContactList contacts = new ContactList();
+			SkipWhitespace(reader);
+			reader.Read(); // <contacts>
+			reader.Read(); // skip <contacts>
+			while (reader.NodeType != XmlNodeType.EndElement)
+			{
+				contacts.Add(ReadContact(reader));
+			}
+			reader.Read(); // </contacts>
+			SkipWhitespace(reader);
+			return contacts;
+		}
+
+		private Contact ReadContact(XmlTextReader reader)
+		{
+			String name = null;
+			String email = null;
+			String tags = null;
+
+			SkipWhitespace(reader);
+			reader.Read(); // <contact>
+			while (name == null || email == null || tags == null)
+			{
+				SkipWhitespace(reader);
+				switch (reader.Name)
+				{
+					case NAME_TAG:
+						name = ReadText(reader);
+						break;
+					case EMAIL_TAG:
+						email = ReadText(reader);
+						break;
+					case TAGS_TAG:
+						tags = ReadText(reader);
+						break;
+				}
+			}
+			reader.Read(); // </contact>
+			SkipWhitespace(reader);
+
+			return new Contact(name, email, tags);
+		}
+
+		private String ReadText(XmlTextReader reader)
+		{
+			SkipWhitespace(reader);
+			reader.Read(); // skip tag
+			String text = reader.Value;
+			reader.Read(); // ending tag
+			reader.Read(); // skip ending tag
+            SkipWhitespace(reader);
+			return text;
+		}
+
+		private void SkipWhitespace(XmlTextReader reader)
+		{
+			while (reader.NodeType == XmlNodeType.Whitespace)
+			{
+				reader.Read();
+			}
 		}
 	}
 }
